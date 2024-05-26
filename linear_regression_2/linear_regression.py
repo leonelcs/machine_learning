658d8f65091dfb85e4697e291875f8772aadbc03
Leonel Candido da Silva <lcandidodasilva@ebay.com>
2024-05-26 09:25:24 +0200

    adding bias to the linear regression

---
+++ linear_regression_2/linear_regression.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LinearRegression:
+    
+    def __init__(self, learning_rate=0.01, n_iters=10000):
+        self.lr = learning_rate
+        print("lr: %4f" % self.lr)
+        self.n_iters = n_iters
+        self.X, self.Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
+
+    def plot(self):
+        
+        plt.scatter(self.X, self.Y)
+        plt.axis([0, 50, 0, 50])                                 # scale axes (0 to 50)
+        plt.xticks(fontsize=14)                                  # set x axis ticks
+        plt.yticks(fontsize=14)
+        plt.xlabel('Reservations', fontsize=14)
+        plt.ylabel('Pizzas', fontsize=14)
+        plt.title('Pizza Deliveries vs Reservations')
+        plt.plot(self.X, self.Y, 'bo')
+        plt.show()
+
+    def train(self):
+        w = b = 0
+        print("iterations: %4d LR: %4f" % (self.n_iters, self.lr))
+        for i in range(self.n_iters):
+            current_loss = self.loss(w, b)
+            print("Interaction %4d => Loss: %6f" % (i, current_loss))
+            # copilot suggested using if instead of elif
+            if self.loss(w + self.lr, b) < current_loss:
+                w += self.lr
+            elif self.loss(w - self.lr, b) < current_loss:
+                w -= self.lr
+            elif self.loss(w, b + self.lr) < current_loss:
+                b += self.lr
+            elif self.loss(w, b - self.lr) < current_loss:
+                b -= self.lr
+            else:
+                return w, b
+
+        raise Exception("Couldn't converge within %d iterations" % self.n_iters)
+
+
+    def predict(self, X, w, b):
+        return X*w+b
+    
+    def loss(self, w, b):
+        # copilot suggested mean instead of average
+        return np.average((self.predict(self.X, w, b) - self.Y) ** 2)
---
+++ linear_regression_2/main.py
@@ -0,0 +1,20 @@
+from linear_regression import LinearRegression
+
+class Main:
+
+    def __init__(self):
+        self._name = 'Main'
+
+    def start(self):
+        #defaults to 0.01 and 2000
+        self.linear_regression = LinearRegression()
+
+if __name__ == '__main__':
+    main = Main()
+    main.start()
+
+    # main.linear_regression.plot()
+
+    w, b = main.linear_regression.train()
+    print("\nw=%.3f and b=%.3f" % (w,b))
+    print("Prediction: x=%d => y=%.2f" % (20, main.linear_regression.predict(20, w, b)))
